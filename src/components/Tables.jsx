b9026da68d4311ce65691af25932c2e7785056d7
SNBhoomika19 <snbhoomika.aiminds@gmail.com>
2025-11-17 16:03:28 +0530

    added table

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -1,14 +1,55 @@
+import { Table } from 'antd';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 const Tables = () => {
+    const tableColumns = [
+        {
+            title: 'Breed Name',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Description',
+            dataIndex: 'description',
+        },
+        {
+            title: 'Life Span(Max)',
+            dataIndex: 'life',
+        },
+
+    ];
+
+    const url = "https://dogapi.dog/api/v2/breeds";
+
+    const [TableData, setTableData] = useState([]);
+
+    const getData = async () => {
+        try {
+            const response = await axios.get(url);
+
+            const mappedData = response.data.data.map((item) => ({
+                key: item.id,
+                name: item.attributes.name,
+                description: item.attributes.description,
+                life: item.attributes.life.max,
+
+            }));
+
+            setTableData(mappedData);
+
+        } catch (error) {
+            console.log("Error fetching data:", error);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     return (
-        <div className="tables-container">
-            "This is Tables Component"
+        <Table dataSource={TableData} columns={tableColumns} />
+    );
+};
 
-        </div>
-
-    )
-
-}
 
 
 export default Tables;
